ac438bc8dca797ac62711d6b30c15fba5cd62ebf
Taylor C. Richberger <Taywee@gmx.com>
2018-06-14 16:26:45 -0600

    some basic stuff is working at the moment

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,8 @@ mod tests {
         book_reviews.insert("Adventures of Huckleberry Finn",    "My favorite book.").unwrap();
         book_reviews.insert("Grimms' Fairy Tales",               "Masterpiece.").unwrap();
         book_reviews.insert("The Adventures of Sherlock Holmes", "Eye lyked it alot.").unwrap();
+        assert_eq!(book_reviews.get("The Adventures of Sherlock Holmes").unwrap(), Some(String::from("Eye lyked it alot.")));
+        assert_eq!(book_reviews.get("The Adventures of Somebody Else").unwrap(), None);
 
         // Test replacement
         assert_eq!(book_reviews.insert("Pride and Prejudice", "Very enjoyable.").unwrap(), None);
@@ -30,16 +32,14 @@ mod tests {
 
         assert!(!book_reviews.is_empty().unwrap());
 
-        assert_eq!(book_reviews.get("The Adventures of Sherlock Holmes").unwrap(), Some(String::from("Eye lyked it alot.")));
-        assert_eq!(book_reviews.get("The Adventures of Somebody Else").unwrap(), None);
-
         assert_eq!(book_reviews.len().unwrap(), 4);
 
         // check for a specific one.
         assert!(!book_reviews.contains_key("Les Misérables").unwrap());
 
         // oops, this review has a lot of spelling mistakes, let's delete it.
-        book_reviews.remove("The Adventures of Sherlock Holmes").unwrap();
+        assert_eq!(book_reviews.remove("The Adventures of Sherlock Holmes").unwrap(), Some(String::from("Eye lyked it alot.")));
+        assert_eq!(book_reviews.remove("The Adventures of Sherlock Holmes").unwrap(), None);
 
         let x: Result<Vec<String>, Error> = book_reviews.keys().unwrap().collect();
         assert_eq!(x.unwrap(), ["Adventures of Huckleberry Finn", "Grimms' Fairy Tales", "Pride and Prejudice"]);
